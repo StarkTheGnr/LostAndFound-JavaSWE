6ea6a8fb0112605b7716201e09b2e92849ae7e20
Abelrahman Gamal-jimy2 <mano.gamal.mg@gmail.com>
2018-12-22 20:38:40 +0200

    Added Services

--- src/ItemService.java
+++ src/ItemService.java
@@ -1,13 +1,21 @@
 
 public class ItemService {
 	Item item = new Item();
-	public boolean EditItem()
+	public boolean EditItem(Item item)
 	{
-		return true;
+		if(ItemRepo.RemoveItem(item.id) &&ItemRepo.SaveItem(item))
+			return true;
+		else
+			return false;
 	}
-	public void CreatItem(String desc, 	String img, Location loc)
+	public Item CreatItem(String desc, 	String img, Location loc, String cat, User u)
 	{
-		
+		Item temp = new Item();
+		temp.setDescription(desc);
+		temp.setImage(img);
+		temp.setLocation(loc);
+		temp.setUser(u);
+		return temp;
 	}
 
 }
