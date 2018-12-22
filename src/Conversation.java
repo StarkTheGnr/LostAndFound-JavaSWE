e53ad1b69bfe740faff08557c3bf52d6f3bfe995
Abelrahman Gamal-jimy2 <mano.gamal.mg@gmail.com>
2018-12-22 20:05:50 +0200

    Added entities

---
+++ src/Conversation.java
@@ -0,0 +1,15 @@
+
+public class Conversation {
+	Message Message;
+	User owner;
+	User finder;
+	public boolean Accept()
+	{
+		
+	}
+	public boolean decline()
+	{
+		
+	}
+
+}
---
+++ src/ItemService.java
@@ -0,0 +1,13 @@
+
+public class ItemService {
+	Item item = new Item();
+	public boolean EditItem()
+	{
+		return true;
+	}
+	public void CreatItem(String desc, 	String img, Location loc)
+	{
+		
+	}
+
+}
